11f03eedd6d026fa71aefa1521d36a89f9078ae7
divyendu <divyendua@gmail.com>
2025-03-27 15:46:36 -0400

    minor fixes

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,17 @@ use paperboy::handlers::rest_handler;
 
 #[tokio::main]
 async fn main() {
-    // let theme = Style::new()
-    //     .background(theme::PRIMARY_BG)
-    //     .color(theme::PRIMARY);
-    // let app = Application::new().window(
-    //     |_| create().style(move |_| theme.clone()),
-    //     Some(
-    //         WindowConfig::default()
-    //             .size(Size::new(1200.0, 900.0))
-    //             .min_size(Size::new(800.0, 600.0))
-    //             .title("Paperboy"),
-    //     ),
-    // );
-    // app.run();
-    rest_handler::load_curl_request().await;
+    let theme = Style::new()
+        .background(theme::PRIMARY_BG)
+        .color(theme::PRIMARY);
+    let app = Application::new().window(
+        |_| create().style(move |_| theme.clone()),
+        Some(
+            WindowConfig::default()
+                .size(Size::new(1200.0, 900.0))
+                .min_size(Size::new(800.0, 600.0))
+                .title("Paperboy"),
+        ),
+    );
+    app.run();
 }
